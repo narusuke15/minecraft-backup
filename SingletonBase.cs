97cb711e7d5112cd5a4fa76a0b1e41256dc1b5a6
narusuke15 <narusuke15@gmail.com>
2013-11-10 23:53:04 +0700

    add singleton base class

---
+++ SingletonBase.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace MCback{
+
+	public abstract class SingletonBase<T> where T : class
+	{
+	  #region Members
+	
+	 
+	  private static readonly Lazy<T> sInstance = new Lazy<T>(() => CreateInstanceOfT());
+	
+	  #endregion
+	
+	  #region Properties
+	
+
+	  public static T instance { get { return sInstance.Value; } }
+	
+	  #endregion
+	
+	  #region Methods
+	
+	  /// <summary>
+	  /// Creates an instance of T via reflection since T's constructor is expected to be private.
+	  /// </summary>
+	  /// <returns></returns>
+	  private static T CreateInstanceOfT()
+	  {
+	    return Activator.CreateInstance(typeof(T), true) as T;
+	  }
+	
+	  #endregion
+	}
+
+}
